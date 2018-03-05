8e925686f45cc9a30b85b3e0840fa58a27b84a31
Daniel Mason <daniel.mason@apolitical.co>
2018-03-05 22:20:10 +0000

    Seperate encoding. Read yaml file into Secret object

--- src/secret/mod.rs
+++ src/secret/mod.rs
@@ -5,23 +5,57 @@ extern crate serde_yaml;
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Metadata {
-    name: String,
-    namespace: Option<String>,
+pub struct Metadata {
+    pub name: String,
+    pub namespace: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Secret {
     #[serde(rename="apiVersion")]
-    api_version: String,
-    kind: String,
-    metadata: Metadata,
+    pub api_version: String,
+    pub kind: String,
+    pub metadata: Metadata,
     #[serde(rename="type")]
-    resource_type: String,
-    data: HashMap<String, String>
+    pub resource_type: String,
+    pub data: HashMap<String, String>
+}
+
+fn horrible_base64_decode(encoded_data: &String) -> String {
+    let raw_data = base64::decode(encoded_data).expect("Base64 data was undecodable");
+    String::from_utf8(raw_data).expect("Base64 data was not a string")
 }
 
 impl Secret {
+
+    /// # Example
+    ///
+    /// ```
+    /// use kubesm::secret::Secret;
+    ///
+    /// let name = "test-name".to_owned();
+    /// let namespace = None;
+    /// let source = [
+    ///         ("SK_THIS_SHOULD_APPEAR".to_owned(), "win".to_owned()),
+    ///         ("THIS_SHOULD_NOT_APPEAR".to_owned(), "fail".to_owned()),
+    ///     ].iter().cloned().collect();
+    ///
+    /// let expected =
+    /// "---
+    /// apiVersion: v1
+    /// kind: Secret
+    /// metadata:
+    ///   name: \"test-name\"
+    ///   namespace: ~
+    /// type: Opaque
+    /// data:
+    ///   THIS_SHOULD_APPEAR: d2lu"
+    ///     .to_owned();
+    ///
+    /// let secret = Secret::new(name, namespace, source);
+    ///
+    /// assert_eq!(expected, secret.to_yaml());
+    /// ```
     pub fn new(name: String, namespace: Option<String>, source: HashMap<String, String>) -> Secret {
         Secret {
             api_version: "v1".to_string(),
@@ -35,44 +69,71 @@ impl Secret {
         }
     }
 
+    /// # Example
+    ///
+    /// ```
+    /// use kubesm::secret::Secret;
+    ///
+    /// let yaml =
+    /// "---
+    /// apiVersion: v1
+    /// kind: Secret
+    /// metadata:
+    ///   name: \"test-name\"
+    ///   namespace: ~
+    /// type: Opaque
+    /// data:
+    ///   THIS_SHOULD_APPEAR: d2lu";
+    ///
+    /// let secret = Secret::from(yaml).unwrap();
+    ///
+    /// assert_eq!("v1", secret.api_version.as_str());
+    /// assert_eq!("win", secret.data.get("THIS_SHOULD_APPEAR").unwrap().as_str());
+    /// ```
+    pub fn from(yaml: &str) -> Result<Secret, serde_yaml::Error> {
+        let encoded_secret : Secret = serde_yaml::from_str(yaml)?;
+        Ok(encoded_secret.decode_data())
+    }
+
     fn get_secrets(source: HashMap<String, String>) -> HashMap<String, String> {
         source.iter()
             // Remove keys that don't start SK_
             .filter(|&(ref key, ref _value)| key.get(..3).unwrap_or_else(|| "") == "SK_".to_string())
-            // Remove SK_ from the key and base64 encode the value
-            .map(|(key, value)| (String::from(&key[3..]), base64::encode(&value)))
+            // Remove SK_ from the key
+            .map(|(key, value)| (String::from(&key[3..]), value.clone()))
             .collect()
     }
 
-    pub fn to_yaml(&self) -> String {
-        serde_yaml::to_string(&self).expect("Could not serialise secret object")
+    fn encode_data(&self) -> Secret {
+        self.with_different_data(
+            self.data.iter()
+                .map(|(key, value)| (key.clone(), base64::encode(&value)))
+                .collect()
+        )
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::Secret;
+    fn decode_data(&self) -> Secret {
+        self.with_different_data(
+            self.data.iter()
+                .map(|(key, value)| (key.clone(), horrible_base64_decode(&value)))
+                .collect()
+        )
+    }
 
-    #[test]
-    fn e2e() {
-        let name = "test-name".to_owned();
-        let namespace = None;
-        let source = [
-            ("SK_THIS_SHOULD_APPEAR".to_owned(), "win".to_owned()),
-            ("THIS_SHOULD_NOT_APPEAR".to_owned(), "fail".to_owned()),
-        ].iter().cloned().collect();
+    fn with_different_data(&self, data: HashMap<String, String>) -> Secret {
+        Secret {
+            api_version: self.api_version.clone(),
+            kind: self.kind.clone(),
+            metadata: Metadata {
+                name: self.metadata.name.clone(),
+                namespace: self.metadata.namespace.clone(),
+            },
+            resource_type: self.resource_type.clone(),
+            data,
+        }
+    }
 
-        let expected =
-"---
-apiVersion: v1
-kind: Secret
-metadata:
-  name: \"test-name\"
-  namespace: ~
-type: Opaque
-data:
-  THIS_SHOULD_APPEAR: d2lu".to_owned();
-git check
-        assert_eq!(expected, Secret::new(name, namespace, source).to_yaml());
+    pub fn to_yaml(&self) -> String {
+        serde_yaml::to_string(&self.encode_data()).expect("Could not serialise secret object")
     }
 }
